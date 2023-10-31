cf590ed04de2820e0f82c76b13927b64feab9c9f
Hussam <hossamelanany1@gmail.com>
2023-11-01 00:08:06 +0200

    added the history and date for invests

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const qty = document.querySelector('.qty')
 const companyName = document.querySelector('.company-name')
 const overAllTotal = document.querySelector('.overall-total')
 
+const overAllTotalHist = document.querySelector('.overall-total-hist')
+
 let earinings;
 if (localStorage.theEarinings != null) {
     earinings = JSON.parse(localStorage.theEarinings)
@@ -21,8 +23,8 @@ function addData() {
         sellingPrice: sellingPrice.value,
         qty: qty.value,
         companyName: companyName.value,
-        total: Math.round((+qty.value * +sellingPrice.value) - (+purchasePrice.value * +qty.value))
-
+        total: Math.round((+qty.value * +sellingPrice.value) - (+purchasePrice.value * +qty.value)),
+        getDate: date.slice(0, 25)
     }
     earinings.push(newProduct)
     localStorage.setItem('theEarinings', JSON.stringify(earinings))
@@ -44,14 +46,16 @@ function showData() {
         <td>${item.sellingPrice}</td>
         <td>${item.qty}</td>
         <td class='caps'>${item.companyName}</td>
+        
         <td>${item.total}</td>
+        <td>${item.getDate}</td>
         <td><button type="button" onclick="
         deleteItem(${i})
         showData()
         " class="btn btn-outline-primary">-</button></td>
         </tr> 
             `
-        console.log(item);
+
     })
 
     let tbody = document.querySelector('.tbody')
@@ -69,22 +73,17 @@ function clearInputs() {
 }
 
 // Get Total
-
-
 function getTotal() {
     let nums = []
     let sum = 0;
     for (let i = 0; i < earinings.length; i++) {
-
         // var num = test[i] + test[i];
         nums.push(+earinings[i].total)
         sum += parseInt(nums[i]);
+
     }
-    overAllTotal.innerHTML = sum
-
+    overAllTotal.innerHTML = ` $${sum}`
     showData()
-
-    console.log(sum);
 }
 getTotal()
 
@@ -95,3 +94,90 @@ function deleteItem(i) {
     getTotal()
     showData()
 }
+
+
+let eariningsHistory;
+if (localStorage.theEariningsHistory != null) {
+    eariningsHistory = JSON.parse(localStorage.theEariningsHistory)
+} else {
+    eariningsHistory = []
+}
+// Push data history
+let pushedData;
+function pushData() {
+    for (let i = 0; i < earinings.length; i++) {
+        let newProduct = {
+            purchasePrice: earinings[i].purchasePrice,
+            sellingPrice: earinings[i].sellingPrice,
+            qty: earinings[i].qty,
+            companyName: earinings[i].companyName,
+            total: earinings[i].total,
+            getDate: earinings[i].getDate
+        }
+        eariningsHistory.push(newProduct)
+        console.log(i);
+    }
+
+    localStorage.removeItem("theEarinings");
+
+    // pushedData = earinings.splice(0, earinings.length)
+    // eariningsHistory.push({ data: pushedData })
+    // eariningsHistory.push({ earinings })
+    // console.log(eariningsHistory);
+
+    localStorage.setItem('theEariningsHistory', JSON.stringify(eariningsHistory))
+    showData()
+    showHistData()
+
+}
+
+// Show Data History
+
+function showHistData() {
+    if (eariningsHistory != []) {
+        let table = '';
+        for (let i = 0; i < eariningsHistory.length; i++) {
+
+            table += `
+        <tr>
+        <th scope="row">${i + 1}</th>
+        <td>${eariningsHistory[i].purchasePrice}</td>
+        <td>${eariningsHistory[i].sellingPrice}</td>
+        <td>${eariningsHistory[i].qty}</td>
+        <td class='caps'>${eariningsHistory[i].companyName}</td>
+        <td>${eariningsHistory[i].total}</td>
+        <td>${eariningsHistory[i].getDate}</td>
+       
+        </tr>
+             `
+            // <td><button type="button" onclick="
+            // deleteItem(${i})
+            // showData()
+            // " class="btn btn-outline-primary">-</button></td>
+            console.log(eariningsHistory[i].purchasePrice);
+        }
+
+        let tbodyHistory = document.querySelector('.tbodyHistory')
+        tbodyHistory.innerHTML = table
+    }
+    showData()
+
+}
+showData()
+showHistData()
+
+//Get Total hist
+function getTotalHist() {
+    let nums = []
+    let sum = 0;
+    for (let i = 0; i < eariningsHistory.length; i++) {
+        // var num = test[i] + test[i];
+        nums.push(+eariningsHistory[i].total)
+        sum += parseInt(nums[i]);
+
+    }
+    overAllTotalHist.innerHTML = ` $${sum}`
+    showData()
+
+}
+getTotalHist()
